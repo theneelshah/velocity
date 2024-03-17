29419adb24538ae151d742bf7d01dc19b546151b
theneelshah <theneelshah@gmail.com>
2024-03-16 17:55:29 -0700

    Add script to push to s3

---
+++ build-server/script.js
@@ -0,0 +1,58 @@
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const mime = require("mime-types");
+
+const s3Client = new S3Client({
+  region: "",
+  credentials: {
+    accessKeyId: "",
+    secretAccessKey: "",
+  },
+});
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
+const init = async () => {
+  console.log("Executing script.js");
+
+  const outDirPath = path.join(__dirname, "output");
+  const command = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+  command.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  command.stdout.on("error", (data) => {
+    console.error("Error", data.toString());
+  });
+
+  command.on("close", async () => {
+    console.log("Build Complete");
+
+    const distDirPath = path.join(__dirname, "output", "dist");
+    const distDirContents = fs.readdirSync(distDirPath, { recursive: true });
+
+    for (const filePath of distDirContents) {
+      if (fs.lstatSync(filePath).isDirectory()) continue;
+
+      console.log("Uploading: ", filePath);
+
+      const cmd = PutObjectCommand({
+        Bucket: "velocity-theneelshah",
+        Key: `__outputs/${PROJECT_ID}/${filePath}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
+
+      await s3Client.send(cmd);
+
+      console.log("Uploaded: ", filePath);
+    }
+
+    console.log("Done...");
+  });
+};
+
+init();
